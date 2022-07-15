4a844622f17c7de886773780ce16b69c8699dc92
jloboguapo <jloboguapo@gmail.com>
2022-07-15 13:53:42 -0700

    set up eslint/prettier and begin making app

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,1 +1,5 @@
-// let's go!
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import StorePicker from './components/StorePicker';
+
+createRoot(document.querySelector('#main')).render(<StorePicker />);
