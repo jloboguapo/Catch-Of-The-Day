05d418c57697ea6e4a5955c0f8a799e7ee5ee823
jloboguapo <jloboguapo@gmail.com>
2022-07-17 02:54:04 -0700

    refactor StorePicker

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { getFunName } from '../helpers';
 
-class StorePicker extends React.Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Please Enter a store</h2>
-        <input
-          type="text"
-          required
-          placeholder="Store Name"
-          defaultValue={getFunName()}
-        />
-        <button type="submit">Visit Store →</button>
-      </form>
-    );
-  }
-}
+const StorePicker = () => {
+  const myInput = React.useRef();
+  const navigate = useNavigate();
+
+  const goToStore = e => {
+    e.preventDefault();
+    navigate(`/store/${myInput.current.value}`);
+  };
+  return (
+    <form className="store-selector" onSubmit={goToStore}>
+      <h2>Please Enter a store</h2>
+      <input
+        type="text"
+        ref={myInput}
+        required
+        placeholder="Store Name"
+        defaultValue={getFunName()}
+      />
+      <button type="submit">Visit Store →</button>
+    </form>
+  );
+};
 
 export default StorePicker;
