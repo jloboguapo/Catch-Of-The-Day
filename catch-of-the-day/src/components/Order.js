b6a8c957396185aeb974bdd98440febfe7139590
jloboguapo <jloboguapo@gmail.com>
2022-07-25 00:17:58 -0700

    add TransitionGroup and CSSTransition to project

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { formatPrice } from '../helpers';
 
 const Order = ({ fishes, order, removeEntireItemFromOrder }) => {
@@ -19,29 +20,56 @@ const Order = ({ fishes, order, removeEntireItemFromOrder }) => {
     const fish = fishes[key];
     const count = order[key];
     const isAvailable = fish && fish.status === 'available';
+    const transitionOptions = {
+      classNames: 'order',
+      key,
+      timeout: { enter: 500, exit: 500 },
+    };
+
     if (!fish) return null;
     if (!isAvailable) {
       return (
-        <li key={key}>
-          Sorry {fish ? fish.name : 'fish'} is no longer available
-        </li>
+        <CSSTransition {...transitionOptions}>
+          <li key={key}>
+            Sorry {fish ? fish.name : 'fish'} is no longer available
+          </li>
+        </CSSTransition>
       );
     }
     if (count > 0)
       return (
-        <li key={key}>
-          {count} lbs {fish.name}
-          {formatPrice(count * fish.price)}
-          <button type="submit" onClick={() => removeEntireItemFromOrder(key)}>
-            &times;
-          </button>
-        </li>
+        <CSSTransition {...transitionOptions}>
+          <li key={key}>
+            <span>
+              <TransitionGroup component="span" className="count">
+                <CSSTransition
+                  classNames="count"
+                  key={count}
+                  timeout={{ enter: 500, exit: 500 }}
+                >
+                  <span>{count}&nbsp;</span>
+                </CSSTransition>
+              </TransitionGroup>
+              lbs {fish.name}
+              <span>&nbsp;{formatPrice(count * fish.price)}</span>
+              <button
+                className="x-button"
+                type="submit"
+                onClick={() => removeEntireItemFromOrder(key)}
+              >
+                &times;
+              </button>
+            </span>
+          </li>
+        </CSSTransition>
       );
   };
   return (
     <div className="order-wrap">
       <h2>Order</h2>
-      <ul className="order">{order && orderIds.map(renderOrder)}</ul>
+      <TransitionGroup component="ul" className="order">
+        {order && orderIds.map(renderOrder)}
+      </TransitionGroup>
       <div className="total">
         Total:
         <strong>{formatPrice(total)}</strong>
