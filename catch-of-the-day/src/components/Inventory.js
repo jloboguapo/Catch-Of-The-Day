c92e5d9b9566931465eb5547c8f4fbf22a53e01c
jloboguapo <jloboguapo@gmail.com>
2022-07-26 03:49:18 -0700

    authenticate owner with firebase database

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/database';
 import PropTypes from 'prop-types';
 import AddFishForm from './addFishForm';
 import EditFishForm from './EditFishForm';
+import Login from './Login';
+import base, { firebaseApp } from '../base';
+import { logout } from '../helpers';
 
 const Inventory = ({
   addFish,
@@ -9,30 +16,80 @@ const Inventory = ({
   fishes,
   updateFish,
   deleteFish,
-}) => (
-  <div className="inventory">
-    <h2>Inventory</h2>
-    {Object.keys(fishes).map(key => (
-      <EditFishForm
-        key={key}
-        index={key}
-        fish={fishes[key]}
-        updateFish={updateFish}
-        deleteFish={deleteFish}
-      />
-    ))}
-    <AddFishForm addFish={addFish} />
-    <button type="submit" onClick={loadSampleFishes}>
-      Load Sample
-    </button>
-  </div>
-);
+  storeId,
+}) => {
+  const [storeOwner, setStoreOwner] = useState({ uid: null, owner: null });
+  const [owner, setOwner] = useState('');
+
+  const authHandler = async authData => {
+    const ownerRef = base.ref(`${storeId}/owner`);
+    await ownerRef.on('value', data => {
+      data.val()
+        ? setOwner(Object.values(data.val())[0])
+        : ownerRef.set({ owner: authData.user.uid });
+    });
+    setStoreOwner({
+      uid: authData.user.uid,
+      owner: owner || authData.user.uid,
+    });
+  };
+
+  const authenticate = provider => {
+    const authProvider = new firebase.auth[`${provider}AuthProvider`]();
+    firebaseApp.auth().signInWithPopup(authProvider).then(authHandler);
+  };
+
+  const loggingOut = async () => {
+    console.log('Logging out!');
+    await firebase.auth().signOut();
+    setStoreOwner({ uid: null });
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      user && authHandler({ user });
+    });
+  }, []);
+
+  if (!storeOwner.uid) {
+    return <Login authenticate={authenticate} />;
+  }
+  if (storeOwner.uid !== storeOwner.owner) {
+    return (
+      <div>
+        <p>Sorry, you are not the owner!</p>
+        {logout(loggingOut)}
+      </div>
+    );
+  }
+  return (
+    <div className="inventory">
+      <h2>Inventory</h2>
+      {logout(loggingOut)}
+      {Object.keys(fishes).map(key => (
+        <EditFishForm
+          key={key}
+          index={key}
+          fish={fishes[key]}
+          updateFish={updateFish}
+          deleteFish={deleteFish}
+        />
+      ))}
+      <AddFishForm addFish={addFish} />
+      <button type="submit" onClick={loadSampleFishes}>
+        Load Sample
+      </button>
+    </div>
+  );
+};
+
 Inventory.propTypes = {
   addFish: PropTypes.func.isRequired,
   loadSampleFishes: PropTypes.func.isRequired,
   fishes: PropTypes.object.isRequired,
   updateFish: PropTypes.func.isRequired,
   deleteFish: PropTypes.func.isRequired,
+  storeId: PropTypes.string.isRequired,
 };
 
 export default Inventory;
