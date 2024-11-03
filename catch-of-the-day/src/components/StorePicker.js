38f6a17fe3a1edf3ab4b48019f430a878046b22e
Juice J <juicej@Alphas-MacBook-Pro.local>
2024-11-03 12:02:54 -0700

    get storepicker up and running

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { getFunName } from '../helpers';
 
 const StorePicker = () => {
+  const myInput = useRef();
+  const navigate = useNavigate();
+
+  const goToStore = e => {
+    const newStore = myInput.current.value;
+    e.preventDefault();
+    navigate(`/store/${newStore}`);
+  };
+
   return (
-    <form className="store-selector">
+    <form className="store-selector" onSubmit={goToStore}>
       <h2>Please Enter A Store</h2>
-      <input type="text" required placeholder="Store Name" />
+      <input
+        type="text"
+        ref={myInput}
+        placeholder="Store Name"
+        required
+        defaultValue={getFunName()}
+      />
       <button type="submit">Visit Store</button>
     </form>
   );
