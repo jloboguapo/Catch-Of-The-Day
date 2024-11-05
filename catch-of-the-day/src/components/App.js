af4278ebb552329e7a490cd1f5cd6e6a9e53a5cc
Juice J <juicej@Alphas-MacBook-Pro.local>
2024-11-05 16:39:18 -0700

    add load sample fishes button

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Order from './Order';
+import sampleFishes from '../sample-fishes';
 
 const App = () => {
   const [fishes, setFishes] = useState({});
@@ -12,13 +13,21 @@ const App = () => {
     setFishes({ ...fishes });
   };
 
+  const loadSampleFishes = () => {
+    setFishes({ ...sampleFishes });
+  };
+
   return (
     <div className="catch-of-the-day">
       <div className="menu">
         <Header tagline="Fresh Seafood Market" />
       </div>
       <Order />
-      <Inventory addFish={addFish} fishes={fishes} />
+      <Inventory
+        addFish={addFish}
+        fishes={fishes}
+        loadSampleFishes={loadSampleFishes}
+      />
     </div>
   );
 };
--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,14 +1,21 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import AddFishForm from './AddFishForm';
 
-const Inventory = ({ addFish }) => {
+const Inventory = ({ addFish, loadSampleFishes }) => {
   return (
     <div className="inventory">
       <h2>Inventory</h2>
       <AddFishForm addFish={addFish} />
+      <button onClick={loadSampleFishes}>Load Sample Fishes</button>
     </div>
   );
+};
+
+Inventory.propTypes = {
+  addFish: PropTypes.func.isRequired,
+  loadSampleFishes: PropTypes.func.isRequired,
 };
 
 export default Inventory;
