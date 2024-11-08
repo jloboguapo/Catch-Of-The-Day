7a84506cf8453c41261e911c181a9a1895403c19
Juice J <juicej@Alphas-MacBook-Pro.local>
2024-11-08 14:00:16 -0700

    fix typo

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -24,7 +24,7 @@ const Fish = ({ details, addToOrder, index, removeFromOrder, order }) => {
       </button>
       {found && (
         <button type="submit" onClick={() => removeFromOrder(index)}>
-          removeFromOrder
+          Remove from Order
         </button>
       )}
     </li>
